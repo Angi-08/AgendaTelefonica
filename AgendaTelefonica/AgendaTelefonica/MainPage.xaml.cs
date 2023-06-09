5c5fb99bb7f1f1b24f78786ca1988f78b001fca0
Angi.benavides <Angi.benavides@CLOUD3>
2023-06-09 10:40:23 -0500

    Agregar archivos de proyecto.

---
+++ AgendaTelefonica/AgendaTelefonica/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using AgendaTelefonica.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AgendaTelefonica
+{
+    public partial class MainPage : TabbedPage
+    {
+        public List<Contacto> lista { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            lista = new List<Contacto>();
+        }
+
+        private void GuardarContacto(object sender, EventArgs e)
+        {
+            Contacto c = new Contacto(Name.Text, LastName.Text, Phone.Text, Cellphone.Text, Email.Text);
+            lista.Add(c);
+            DisplayAlert("Contacto agregado con exito", "", "Aceptar");
+            listado.ItemsSource = null;
+            listado.ItemsSource = lista;
+            Name.Text = string.Empty;
+            LastName.Text = string.Empty;
+            Phone.Text = string.Empty;
+            Cellphone.Text = string.Empty;
+            Email.Text = string.Empty;
+
+        }
+    }
+}
---
+++ AgendaTelefonica/AgendaTelefonica/Model/Contacto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgendaTelefonica.Model
+{
+    public class Contacto
+    {
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string Telefono { get; set; }
+
+        public string Celular { get; set; }
+
+        public string Email { get; set; }
+
+        public Contacto(string nombre, string apellido, string telefono, string celular, string email)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Telefono = telefono;
+            Celular = celular;
+            Email = email;
+        }
+    }
+}
